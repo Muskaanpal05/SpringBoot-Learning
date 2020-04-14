b72bfebd6fd7bdc41ee9c137074c791e7ecf6df8
Muskaanpal05 <palmuskaan5@gmail.com>
2020-04-14 13:18:47 +0530

    Returning list of object from controller

--- src/main/java/muskaan/springboot/learning/TopicController.java
+++ src/main/java/muskaan/springboot/learning/TopicController.java
@@ -3,13 +3,24 @@ package muskaan.springboot.learning;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Arrays;
+import java.util.List;
+
 
 @RestController
 public class TopicController{
 
     @RequestMapping("/topics")
-    public String getTopics(){
-        return "All Topics";
+    public List<Topic> getAllTopics(){
+
+        return Arrays.asList(
+         new Topic("spring", "Spring Framework" , "Spring Framework Description"),
+
+        new Topic("java", "Core java" , "Core Java Description"),
+
+        new Topic("javascript", "javascript" , "Javascript Description")
+
+        );
     }
 
 }
