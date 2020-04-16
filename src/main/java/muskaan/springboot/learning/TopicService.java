6861ecd66ee5c1fb75af1ca8ad2cafdb1d201070
Muskaanpal05 <palmuskaan5@gmail.com>
2020-04-16 15:15:09 +0530

    Creating a new resource using POST

--- src/main/java/muskaan/springboot/learning/TopicService.java
+++ src/main/java/muskaan/springboot/learning/TopicService.java
@@ -2,26 +2,31 @@ package muskaan.springboot.learning;
 
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 @Service
 public class TopicService {
 
-  private List<Topic> topics=Arrays.asList(
+  private List<Topic> topics=new ArrayList<>(Arrays.asList(
           new Topic("spring", "Spring Framework" , "Spring Framework Description"),
 
         new Topic("java", "Core java" , "Core Java Description"),
 
         new Topic("javascript", "javascript" , "Javascript Description")
 
-        );
+        ));
   public List<Topic> getAllTopics(){
       return topics;
   }
 
   public Topic getTopic(String id){
       return topics.stream().filter(t-> t.getId().equals(id)).findFirst().get();
+  }
+
+  public void addTopic(Topic topic){
+      topics.add(topic);
   }
 
 }
